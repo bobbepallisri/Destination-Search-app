ddc2b1b5244def9493314c8625eae35ab879edfa
bobbepallisri <srikanthbobbepalli3@gmail.com>
2024-01-30 09:29:53 +0000

    solutions

---
+++ src/components/DestinationSearch/index.js
@@ -0,0 +1,56 @@
+import {Component} from 'react'
+
+import DestinationItem from '../DestinationItem'
+
+import './index.css'
+
+class DestinationSearch extends Component {
+  state = {
+    searchInput: '',
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  render() {
+    const {searchInput} = this.state
+    const {destinationsList} = this.props
+    const searchResults = destinationsList.filter(eachDestination =>
+      eachDestination.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+
+    return (
+      <div className="Container">
+        <div className="Sub-container">
+          <h1 className="heading">Destination Search</h1>
+
+          <div className="search-container">
+            <input
+              type="search"
+              className="search"
+              placeholder="Search"
+              value={searchInput}
+              onChange={this.onChangeSearchInput}
+            />
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+              alt="search icon"
+              className="icon"
+            />
+          </div>
+          <ul className="dest-list">
+            {searchResults.map(eachDestination => (
+              <DestinationItem
+                key={eachDestination.id}
+                destinationDetails={eachDestination}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default DestinationSearch
